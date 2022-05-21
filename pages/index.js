b233c1255782f44f7641b9446444ca19437c9dee
usameyuce <avusameyuce@outlook.com>
2022-05-21 21:11:40 +0200

    rembrandt

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+import Link from "next/link";
+import RecipeCard from "../components/RecipeCard";
+
+export const getStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+  const res = await client.getEntries({ content_type: "recipe" }); // if we will add new content type we have to change this part
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
+};
+
+export default function Recipes({ recipes }) {
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe}></RecipeCard>
+      ))}
+      <style jsx>{`
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px 60px;
+      `}</style>
     </div>
-  )
+  );
 }
